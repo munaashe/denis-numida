599a881c0b90b8ab332ab737e7dabd6e3b489a58
Denis Siduna <denis.siduna@gmail.com>
2024-08-27 20:50:37 +0200

    dashboard page config

--- frontend/app/(tabs)/explore.tsx
+++ frontend/app/(tabs)/explore.tsx
@@ -1,15 +1,29 @@
-import { StyleSheet} from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import Button from '@/components/button';
+import { useRouter } from 'expo-router';
 
 export default function TabTwoScreen() {
+  const router = useRouter();
+  const handlePress = () => {
+    router.push('/');
+  };
   return (
-   
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Good to go!</ThemedText>
-      </ThemedView>
+
+    <ThemedView style={styles.titleContainer}>
+      <ThemedText type="title">Good to go!</ThemedText>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="APPLY FOR LOAN"
+          onPress={handlePress}
+          variant="solid"
+          style={styles.buttonStyle}
+        />
+      </View>
+    </ThemedView>
 
   );
 }
@@ -22,5 +36,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     gap: 8,
+  },
+  buttonContainer: {
+    padding: 30,
+    backgroundColor: '#FFFFFF',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
+  buttonStyle: {
+    alignSelf: 'stretch',
+    marginBottom: 28,
+    textTransform: 'uppercase',
   },
 });
--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,33 +1,81 @@
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import Button from '@/components/button';
+import PageTitle from '@/components/page-title';
+import LoanCard from '@/components/loan-card';
 
 export default function HomeScreen() {
+  const router = useRouter();
+  const handlePress = () => {
+    router.push('/explore');
+  };
+
   return (
-    <ThemedView style={styles.titleContainer}>
-      <ThemedText type="title">And Here we go!</ThemedText>
-    </ThemedView>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.content}>
+        <PageTitle
+          title="Loan Application Dashboard"
+          style={styles.pageTitle}
+        />
+        <LoanCard
+          title="Personal Loan"
+          amount="$200,000"
+          interest="Interest: 3.5%"
+          onClick={handlePress}
+          style={styles.blueCardStyle}
+        />
+        <LoanCard
+          title="Home Loan"
+          amount="$20,000"
+          interest="Interest: 5%"
+          onClick={handlePress}
+        />
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="APPLY FOR LOAN"
+          onPress={handlePress}
+          variant="solid"
+          style={styles.buttonStyle}
+        />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    height: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  content: {
+    padding: 30,
+    paddingTop: 80,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
+  pageTitle: {
+    width: '75%',
+    paddingTop: 62,
+    lineHeight: 34,
+    fontSize: 29,
+    fontWeight: 'bold',
+    fontFamily: 'RobotoBlack',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    padding: 30,
+    backgroundColor: '#FFFFFF',
     position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
+  buttonStyle: {
+    alignSelf: 'stretch',
+    marginBottom: 28,
+    textTransform: 'uppercase',
+    height: 56,
+  },
+  blueCardStyle: {
+    backgroundColor: '#D4FAF5',
+    marginBottom: 20,
   },
 });
