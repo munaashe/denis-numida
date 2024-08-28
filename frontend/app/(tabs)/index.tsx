ec02f543e1451cef2267db334b49023bfa137634
Denis Siduna <denis.siduna@gmail.com>
2024-08-28 09:08:27 +0200

    add graphql requests

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,15 +1,22 @@
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import { useRouter } from 'expo-router';
 import Button from '@/components/button';
 import PageTitle from '@/components/page-title';
 import LoanCard from '@/components/loan-card';
+import { GET_LOAN_PRODUCTS } from '@/graphql/queries';
+import { useQuery } from '@apollo/client';
+import client from '@/graphql/client';
+import { Key } from 'react';
 
 export default function HomeScreen() {
   const router = useRouter();
   const handlePress = () => {
     router.push('/explore');
   };
+  const { loading, error, data } = useQuery(GET_LOAN_PRODUCTS, { client });
 
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error loading loan products</Text>;
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.content}>
@@ -17,19 +24,16 @@ export default function HomeScreen() {
           title="Loan Application Dashboard"
           style={styles.pageTitle}
         />
-        <LoanCard
-          title="Personal Loan"
-          amount="$200,000"
-          interest="Interest: 3.5%"
-          onClick={handlePress}
-          style={styles.blueCardStyle}
-        />
-        <LoanCard
-          title="Home Loan"
-          amount="$20,000"
-          interest="Interest: 5%"
-          onClick={handlePress}
-        />
+
+        {data?.loanProducts?.map((loan: { id: Key | null | undefined; name: string; maximumAmount: any; interestRate: any; }) => (
+          <LoanCard
+            key={loan.id}
+            title={loan.name}
+            amount={`$${loan.maximumAmount}`}
+            interest={loan.interestRate}
+            onClick={() => { }}
+          />
+        ))}
       </ScrollView>
       <View style={styles.buttonContainer}>
         <Button
